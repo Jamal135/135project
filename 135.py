b2363a70807e47193d10a24a145a350ee7e169b8
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2023-10-24 04:55:45 +1000

    Implemented rate limiting

--- 135.py
+++ 135.py
@@ -5,13 +5,21 @@ from functions.cipher_135 import encrypt_135, decrypt_135
 from functions.cipher_101 import encrypt_101, decrypt_101
 from functions.basetool import base_convert
 from functions.counttool import count_analysis
-from flask import Flask, render_template, send_from_directory, jsonify
+from flask import Flask, render_template, send_from_directory, jsonify, redirect, url_for
 from flask_wtf.csrf import CSRFProtect
 from os import path, urandom
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
-# Set up CSRF protection.
+# Setup app.
 csrf = CSRFProtect()
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["500 per day", "200 per hour"],
+    storage_uri='memory://'
+)
 app.config['SECRET_KEY'] = urandom(32)
 app.config['WTF_CSRF_TIME_LIMIT'] = None
 csrf.init_app(app)
@@ -22,19 +30,18 @@ def favicon():
     return send_from_directory(path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-# --Main pages--
+# Main pages
 @app.route("/", methods=["GET"])
 @app.route("/home", methods=["GET"])
 def home():
     return render_template('other/home.html', title='Home')
 
-# --Encryption pages--
+# Encryption pages
 @app.route("/encryption", methods=["GET"])
 @app.route("/encryption/viewall", methods=["GET"])
 def encryption_viewall():
     return render_template('encryption/viewall.html', title="Encryption")
 
-# 135Cipher.
 @app.route("/encryption/135cipher", methods=['GET'])
 def cipher135():
     return render_template('encryption/135cipher.html', title="135Cipher")
@@ -68,7 +75,6 @@ def cipher135_result():
 def cipher135_about():
     return render_template('encryption/135cipher-about.html', title="135Cipher")
 
-# 147Cipher.
 @app.route("/encryption/147cipher", methods=['GET'])
 def cipher147():
     return render_template('encryption/147cipher.html', title="147Cipher")
@@ -99,7 +105,6 @@ def cipher147_result():
 def cipher147_about():
     return render_template('encryption/147cipher-about.html', title="147Cipher")
 
-# 101Cipher.
 @app.route("/encryption/101cipher", methods=["GET"])
 def cipher101():
     return render_template('encryption/101cipher.html', title="101Cipher")
@@ -128,13 +133,12 @@ def cipher101_result():
 def cipher101_about():
     return render_template('encryption/101cipher-about.html', title="101Cipher")
 
-# --Tool pages--
+# Tool pages
 @app.route("/datatools", methods=["GET"])
 @app.route("/datatools/viewall", methods=["GET"])
 def datatools_viewall():
     return render_template('datatools/viewall.html', title="Data Tools")
 
-# Base tool.
 @app.route("/datatools/basetool", methods=["GET"])
 def basetool():
     return render_template('datatools/basetool.html', title="Base Tool")
@@ -163,7 +167,6 @@ def basetool_result():
 def basetool_about():
     return render_template('datatools/basetool-about.html', title="Base Tool")
 
-# Count tool.
 @app.route("/datatools/counttool", methods=["GET"])
 def counttool():
     return render_template('datatools/counttool.html', title="Count Tool")
@@ -190,7 +193,7 @@ def counttool_result():
 def counttool_about():
     return render_template('datatools/counttool-about.html', title="Count Tool")
 
-# --Other pages--
+# Other pages
 @app.route("/disclaimer", methods=["GET"])
 def disclaimer():
     return render_template('other/disclaimer.html', title="Disclaimer")
@@ -203,12 +206,12 @@ def settings():
 def privacy():
     return render_template('other/privacy.html', title="Privacy")
 
-# --Error handling--
+# Error handling
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('other/home.html', title='Home'), 404
 
-# Security Headers
+# Security headers
 @app.after_request
 def add_header(response):
     response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
@@ -218,6 +221,6 @@ def add_header(response):
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
     return response
 
-# Debug mode.
+# Debug mode
 if __name__ == "__main__":
     app.run(debug=True)
